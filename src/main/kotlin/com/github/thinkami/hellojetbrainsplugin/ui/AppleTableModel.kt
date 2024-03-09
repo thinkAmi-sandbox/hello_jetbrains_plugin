3dabe5194dde2c265af98ce75ab2ba25246b9d7e
thinkAmi <dev.thinkami@gmail.com>
2024-03-09 10:43:50 +0900

    add static filter

--- src/main/kotlin/com/github/thinkami/hellojetbrainsplugin/ui/AppleTableModel.kt
+++ src/main/kotlin/com/github/thinkami/hellojetbrainsplugin/ui/AppleTableModel.kt
@@ -2,17 +2,23 @@ package com.github.thinkami.hellojetbrainsplugin.ui
 
 import javax.swing.table.AbstractTableModel
 
-class AppleTableModel: AbstractTableModel() {
+class AppleTableModel : AbstractTableModel() {
     val allData: List<List<String>> = listOf(
         listOf("1", "シナノゴールド", "黄"),
         listOf("2", "シナノホッペ", "赤"),
         listOf("3", "ジョナゴールド", "赤"),
     )
-
     val columns: List<String> = listOf("No", "Name", "Color")
 
+    lateinit var tableData: List<List<String>>
+
+    init {
+        // 初期段階で絞り込みを実行する
+        filterChanged()
+    }
+
     override fun getRowCount(): Int {
-        return allData.size
+        return tableData.size
     }
 
     override fun getColumnCount(): Int {
@@ -20,6 +26,17 @@ class AppleTableModel: AbstractTableModel() {
     }
 
     override fun getValueAt(rowIndex: Int, columnIndex: Int): Any {
-        return allData[rowIndex][columnIndex]
+        return tableData[rowIndex][columnIndex]
+    }
+
+    fun filterChanged() {
+        tableData = allData.filter {
+            val name = it[1] // Nameで絞り込むため、列番号を指定
+            val regex = Regex("シナノ")
+            regex.containsMatchIn(name)
+        }.toList() // allDataとは別オブジェクトにするため toList する
+
+        // filterが更新されたことを通知する
+        this.fireTableDataChanged()
     }
 }
